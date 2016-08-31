1532710ed2503357e90ef29b3b6b35e49b870f1a
guolinqi <294354693@qq.com>
2016-08-31 16:43:12 +0800

    okhttp

---
+++ YuWeiFood/app/src/main/java/com/example/dllo/yuweifood/OKHttp/OKHttpUtil.java
@@ -0,0 +1,235 @@
+package com.example.dllo.yuweifood.OKHttp;         /*
+                                MMMMM
+                                  MMMMMM
+                                    MMMMMMM
+                                     MMMMMMMM     .
+                                      MMMMMMMMM
+                                      HMMMMMMMMMM
+                                       MMMMMMMMMMMM  M
+                                       MMMMMMMMMMMMM  M
+                                        MMMMMMMMMMMMM  M
+                                        MMMMMMMMMMMMM:
+                                        oMMMMMMMMMMMMMM
+              .MMMMMMMMMMMMMMo           MMMMMMMMMMMMMMM M
+        MMMMMMMMMMMMMMMMMMMMMMMMMMM      MMMMMMMMMMMMMMMM
+          MMMMMMMMMMMMMMMMMMMMMMMMMMMM.  oMMMMMMMMMMMMMMM.M
+            MMMMMMMMMMMMMMMMMMMMMMMMMMMM  MMMMMMMMMMMMMMMM
+              MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+                oMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+                  MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+                    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM:                     H
+                     MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM                  .         MMM
+                      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM              M       MMMMMM
+                       .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM          M   MMMMMMMMMM
+                MM.      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM       M MMMMMMMMMMMM
+                    MM    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM    .MMMMMMMMMMMMMM
+                      MM  MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+                        MM MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+               .MMMMMMMMM MMMMMMMMMMMMMMMMMMMMMMMM.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+                  HMMMMMMMMMMMMMMMMMMMMM.MMMMMMMMM.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+                     MMMMMMMMMMMMMMM MMM.oMMMMMMM..MMMMMMMMM:MMMMMMMMMMMMMMMMMMMMMMM
+                       MMMMMMMMMMMMMM MM..MMMMMMM...MMMMMMM. MMMMMMMMMMMMMMMMMMMMM
+                         MMMMMMMMMMMMMMM ..MMMMMM...MMMMMM ..MMMMMMMMMMMMMMMMMMM
+                          MMMMMMM:M.MMM.M.. MMMMM M..MMMMM...MMMMMMMMMMMMMMMMMM  MMM
+                            MMMM. .M..MM.M...MMMMMM..MMMMM.. MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM .
+                             MMMM..M....M.....:MMM .MMMMMM..MMMMMMM...MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+                              MMM.M.. ...M......MM.MMMMM.......MHM.M  .MMMMMMMMMMMMMMMMMMMMMMMMM
+                         MMMMMMMM..MM. . MMM.....MMMMMM.M.....M ..MM..M MMMMMMMMMMMMMMMMMMM
+                            .MMMMMHMM. ..MMMM. MMM............o..... . .MMMMMMMMMMMMMMM
+                               MMM. M... .........................M..:.MMMMMMMMMMMM
+                                 oMMM............ .................M.M.MMMMMMMMM
+                                    .....MM........................ . MMMMMM
+                                   M.....M.....................o.MM.MMMMMMMM.
+                                    M........................M.. ...MMMMMMMMMMMMMo
+                                      :....MMM..............MMM..oMMMMMMM
+                                       M...MMM.............MMMMMMM
+                                          .............:MMMMMMMM
+                                          M..... MMM.....M
+                                          M M.............
+                                          ................M
+                                       ooM.................MM  MoMMMMMoooM
+                                  MMoooM......................MoooooooH..oMM
+                              MHooooMoM.....................MMooooooM........M
+                            oooooooMoooM......... o........MoooooooM............
+                            Mooooooooooo.......M.........Moooooooo:..............M
+                           MooMoooooooooM...M........:Mooooooooooo:..............M
+                          M..oooooooooooo .........Mooooooooooooooo..............M
+                         M...Mooo:oooooooo.M....ooooooooooooooooooo..M...........M
+                          ...oooooMoooooooM..Mooooooooooooo:oooooooM.M...........M.
+                         M...ooooooMoo:ooooMoooooooooooooHoooooooooH:M. ...........:
+                         M..MoooooooMoooooooooooooooooo:ooooooMooooMoM..............M
+                         M..ooooooooooMooooooooooooooHoooooooMooHooooM...............M
+                         ...ooooooooooooooooooo:MooooooooooooooMoMoooM................
+                        M...oooooooooooooooooooooooooooooooooooooMooMM................M
+                        ...MooooooooooooooooooooooooooooooooooooooooMo ................
+                        ...MooooooooooooooooooooooooooooooooooooooooM M................M
+                       M...ooooooooooooooooooooooooooooooooooooooooM   ................M
+                       ...MoooooooooooooooooooooooooooooooooooooooMM   .:...............
+                       .....MooooooooooooooooooooooooooooooooooooMoo       .............M
+                       M...... ooooooooooooooooooooooooooooooooooooM       M..............M
+                       M........MooooMMM MM MM  MMMMMMMMMooooooooM         M...............M
+                       .........HM     M:  MM :MMMMMM          M           M...............
+                      M..........M     M   MoM M                           M................M
+                      M.........:M  MoH  M M M MooooHoooMM.   M             M...............M
+                      M..........Moooo MMooM    oooooMooooooooM              M..............H
+                      M.........MooooM  Mooo  : ooooooMooooMoooM              M........ . .o.M
+                      H..  .....ooooo   oooo  M MooooooooooooooM               M... MMMMMMMMMMM
+                      MMMMMMMMMMooooM M oooo  .  ooooooMooooooooM              .MMMMMMMMMMMMMMM
+                      MMMMMMMMMMooooH : ooooH    oooooooooooooooo               MMMMMMMMMMMMMMM
+                      MMMMMMMMMMoooo    ooooM    Moooooooooooooooo              .MMMMMMMMMMMMMMM
+                      MMMMMMMMMMoooo    ooooM    MooooooooooooooooM              MMMMMMMMMMMMMMM
+                      MMMMMMMMMMoooM    ooooM     ooooooooooooooooo               MMMMMMMMMMM:M
+                      MMMMMMMMMMoooM   MooooM     oooooooooooMoooooo               MH...........
+                       . ......Mooo.   MooooM     oooooooooooooooooo              M............M
+                      M.M......oooo    MooooM     Moooooooooooooooooo:           .........M.....
+                      M.M.....Moooo    MooooM      ooooooooooooooooooM            .M............
+                      .......MooooH    MooooM      oooooooooMoooooooooo          M..o...M..o....M
+                      .o....HMooooM    MooooH      MooooooooMooooooooooM          .:M...M.......M
+                     M..M.....MoooM    :oooo:    .MooooooooHooMoooooooooM         M M... ..oM.M
+                      M...M.:.Mooo. MMMMooooo   oooooooooooMoooooooooooooM          ....M. M
+                       M:M..o.Moooooooooooooo MooooooooooooooMooooooooooooM          .Mo
+                              MooooooooooooooMooooooooooooMoMoooooooooooooo
+                              Mooooooooooooooo:ooooooooooooooooooooooooooooo
+                              ooooooooooooooooMooooooooooMoooooooooooooooooo
+                              ooooooooooooooooMoooooooooooMooooooooooooooooHo
+                              ooMooooooooooooooMoooooooooooooooooooooooooooMoM
+                             MooMoooooooooooooo.ooooooooooooooooooooooooooo:oM
+                             MoooooooooooooooooooooooooooooooooooooooooooooooM
+                             MoooMooooooooooooooMooooooooooooooooooooooooooooo.
+                             MoooMooooooooooooooMoooooooooooooooooooooooooMooooM
+                             MooooooooooooooooooMoooooooooooooooooooooooooMoooooM
+                             MooooMoooooooooooooMoooooooooooooooooooooooooMoHooooM
+                             ooooooMooooooooooooooooooooooooooooooooooooooooMoMoooM
+                            MooooooooooooooooooooMooooooooooooooooooooooooooMoooooH:
+                            MoooooooMooooooooooooMoooooooooooooooooooooooooooooHoooM
+                            MooooooooMoooooooooooMoooooooooooooooooooooooooMoooMooooM
+                            Moooooooooooooooooooooooooooooooooooooooooooooo.oooMooooo
+                            MoooooooooooooooooooooooooooooooooooooooooooooMoooooooooM
+                             MooooooooooooooooooooMoooooooooooooooooooooooooooooooooM
+                              MooooooooooooooooooooMHooooooooooooooooooooMoooo:ooooo
+                               MMooooooooooooooooooMoMHoooooooooooooooooooooooMooooo
+                                MMoooooooooooooooMMooo MMooooooooooooooooooooooooooM
+                                MMMoooooooooooooMooooo  oooooooooooooooooooooMooooo
+                                MooMMoooooooooMoooMMoM  ooooHooooooooooooooooMooooM
+                                MooooMooooooMooooMoooM  MoooooMoooooooooooooMooooo
+                                ooooooMMooooooooMooooM  MoooooooooMooooooooooooooM
+                                HooooooMoooooooMooooM    HoooooooHooMooooooooooooo
+                                 oooMoooooooooHoooM         MoooooooooMoooooooooM
+                                  HooooooooooooHM             MooooooooMMoooooooM
+                                   MMMMMMMMMMMMMM                Moooooo:MooooHMM
+                                    MMMMMMM: ...                  MMMMMMMMMMMMMM
+                                   M............M                  MMMMMMMMM ....
+                                   M.MM..........                  M.............M
+                                M ..............MM                 M..............
+                             MMMMM............MMMM                 ..MMMMMMMM ....M
+                           MMMMMMMMMMMMMMMMMMMMMMMM               MMMMMMMMMMMMM...M
+                        .MMMMMMMMMMMMMMMMMMMMMMMMMM               MMMMMMMMMMMMMMMMMM
+                        MMMMMMMMMMMMMMMMMMMMMMMMM                MMMMMMMMMMMMMMMMMMM
+                        :MMMMMMMMMMMMMMMMMMH                     MMMMMMMMMMMMMMMMMMM
+                           By ___Guo&N                           MMMMMMMMMMMMMMMMMM
+                                                                 MMMMMMMMMMMMMMM
+                                                                    HMMMMMM
+                  If time could stop at the moment when we first met.                                             
+                                                        ------- To you.
+        */
+
+
+
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+
+
+
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import okhttp3.Call;
+import okhttp3.Cache;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+public class OKHttpUtil implements NetInterface{
+    private OkHttpClient mClient;
+    //车洋定义一个Handler对象 无论在哪里创建的 都是属于主线程的
+    private Handler handler = new Handler(Looper.getMainLooper());
+    private Gson gson;
+
+    public OKHttpUtil() {
+        super();
+        gson = new Gson();
+        //设置缓存 获取系统的SD卡
+        File path = Environment.getExternalStorageDirectory();
+        //初始化okhttp
+                                                    //  设置内存大小
+        mClient = new OkHttpClient().newBuilder().cache(new Cache(path,10*1024*1024))
+                                //设置超时时间
+                .connectTimeout(5, TimeUnit.SECONDS)
+                .build();
+    }
+
+    @Override
+    public void startRequest(String url, final onHttpCallBack<String> callBack) {
+
+        Request request = new Request.Builder().url(url).build();
+        mClient.newCall(request).enqueue(new Callback() {
+
+
+            @Override
+            public void onFailure(Call call, final IOException e) {
+                //失败回调时
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callBack.onError(e);
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String result = response.body().string();
+                //成功回调时  我们将任务传递给主线程
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callBack.onSuccess(result);
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public <T> void startRequest(String url, final Class<T> tClass, final onHttpCallBack<T> Callback) {
+        Request request = new Request.Builder().url(url).build();
+        mClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, final IOException e) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Callback.onError(e);
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                    String str = response.body().string();
+                final T result = gson.fromJson(str,tClass);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Callback.onSuccess(result);
+                    }
+                });
+            }
+        });
+    }
+}
