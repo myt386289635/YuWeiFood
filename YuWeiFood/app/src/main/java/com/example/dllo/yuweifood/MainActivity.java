2fdcc8e37d9217c49bc8c9451977f2c772b4e491
莫玉婷 <dllo@moyuting.local>
2016-08-31 19:19:20 +0800

    框架修改

--- YuWeiFood/app/src/main/java/com/example/dllo/yuweifood/MainActivity.java
+++ YuWeiFood/app/src/main/java/com/example/dllo/yuweifood/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.example.dllo.yuweifood.food.FoodFragment;
 import com.example.dllo.yuweifood.local.LocalFragment;
@@ -17,7 +18,10 @@ import com.example.dllo.yuweifood.recommend.RecommendFragment;
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private RadioButton mRadioButton_recommend,mRadioButton_local,mRadioButton_food,mRadioButton_mine;
+    private TextView mTextView_recommend,mTextView_local,mTextView_food,mTextView_mine;
 
+    private RadioButton[] mRadioButtons;
+    private TextView[] mTextViews;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,11 +31,18 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         mRadioButton_local = (RadioButton) findViewById(R.id.btn_local);
         mRadioButton_food = (RadioButton) findViewById(R.id.btn_food);
         mRadioButton_mine = (RadioButton) findViewById(R.id.btn_mine);
+        mTextView_recommend = (TextView) findViewById(R.id.btn_recommend_text);
+        mTextView_local = (TextView) findViewById(R.id.btn_local_text);
+        mTextView_food = (TextView) findViewById(R.id.btn_food_text);
+        mTextView_mine = (TextView) findViewById(R.id.btn_mine_text);
 
         mRadioButton_recommend.setOnClickListener(this);
         mRadioButton_food.setOnClickListener(this);
         mRadioButton_mine.setOnClickListener(this);
         mRadioButton_local.setOnClickListener(this);
+
+        mRadioButtons = new RadioButton[]{mRadioButton_recommend, mRadioButton_local, mRadioButton_food, mRadioButton_mine};
+        mTextViews  = new TextView[]{mTextView_recommend, mTextView_local, mTextView_food, mTextView_mine};
 
         replaceFragment(new RecommendFragment());
     }
@@ -42,19 +53,57 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         switch (v.getId()){
             case R.id.btn_recommend:
 
+                for (int i = 0; i < mRadioButtons.length; i++) {
+                    if(i == 0){
+                        mRadioButtons[i].setChecked(true);
+                        mTextViews[i].setTextColor(0xffffa12c);
+                    }else {
+                        mRadioButtons[i].setChecked(false);
+                        mTextViews[i].setTextColor(0xff000000);
+                    }
+
+                }
                 replaceFragment(new RecommendFragment());
                 break;
 
             case R.id.btn_local:
 
+                for (int i = 0; i < mRadioButtons.length; i++) {
+                    if(i == 1){
+                        mRadioButtons[i].setChecked(true);
+                        mTextViews[i].setTextColor(0xffffa12c);
+                    }else {
+                        mRadioButtons[i].setChecked(false);
+                        mTextViews[i].setTextColor(0xff000000);
+                    }
+
+                }
                 replaceFragment(new LocalFragment());
                 break;
             case R.id.btn_food:
+                for (int i = 0; i < mRadioButtons.length; i++) {
+                    if(i == 2){
+                        mRadioButtons[i].setChecked(true);
+                        mTextViews[i].setTextColor(0xffffa12c);
+                    }else {
+                        mRadioButtons[i].setChecked(false);
+                        mTextViews[i].setTextColor(0xff000000);
+                    }
 
+                }
                 replaceFragment(new FoodFragment());
                 break;
             case R.id.btn_mine:
+                for (int i = 0; i < mRadioButtons.length; i++) {
+                    if(i == 3){
+                        mRadioButtons[i].setChecked(true);
+                        mTextViews[i].setTextColor(0xffffa12c);
+                    }else {
+                        mRadioButtons[i].setChecked(false);
+                        mTextViews[i].setTextColor(0xff000000);
+                    }
 
+                }
                 replaceFragment(new MineFragment());
                 break;
         }
